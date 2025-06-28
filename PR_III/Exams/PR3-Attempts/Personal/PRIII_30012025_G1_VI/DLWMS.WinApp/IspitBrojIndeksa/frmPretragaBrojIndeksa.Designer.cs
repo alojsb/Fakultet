ca75a90d4d72a84805325314075eeb50fea62b9e
alojsb <58426780+alojsb@users.noreply.github.com>
2025-06-28 18:28:01 +0200

    PRIII_30012025_G1_VI
    25 bodova

---
+++ PR_III/Exams/PR3-Attempts/Personal/PRIII_30012025_G1_VI/DLWMS.WinApp/IspitBrojIndeksa/frmPretragaBrojIndeksa.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace DLWMS.WinApp.IspitBrojIndeksa
+{
+    partial class frmPretragaBrojIndeksa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtImePrezime = new TextBox();
+            cmbDrzava = new ComboBox();
+            cmbSpol = new ComboBox();
+            dgvStudenti = new DataGridView();
+            colImePrezime = new DataGridViewTextBoxColumn();
+            colDrzava = new DataGridViewTextBoxColumn();
+            colGrad = new DataGridViewTextBoxColumn();
+            colSpol = new DataGridViewTextBoxColumn();
+            colAktivan = new DataGridViewCheckBoxColumn();
+            colRazmjene = new DataGridViewButtonColumn();
+            lblImePrezime = new Label();
+            lblDrzava = new Label();
+            lblSpol = new Label();
+            ((System.ComponentModel.ISupportInitialize)dgvStudenti).BeginInit();
+            SuspendLayout();
+            // 
+            // txtImePrezime
+            // 
+            txtImePrezime.Location = new Point(12, 39);
+            txtImePrezime.Name = "txtImePrezime";
+            txtImePrezime.Size = new Size(189, 23);
+            txtImePrezime.TabIndex = 0;
+            txtImePrezime.KeyUp += txtImePrezime_KeyUp;
+            // 
+            // cmbDrzava
+            // 
+            cmbDrzava.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbDrzava.FormattingEnabled = true;
+            cmbDrzava.Location = new Point(207, 39);
+            cmbDrzava.Name = "cmbDrzava";
+            cmbDrzava.Size = new Size(181, 23);
+            cmbDrzava.TabIndex = 1;
+            cmbDrzava.SelectionChangeCommitted += cmbDrzava_SelectionChangeCommitted;
+            // 
+            // cmbSpol
+            // 
+            cmbSpol.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbSpol.FormattingEnabled = true;
+            cmbSpol.Location = new Point(394, 39);
+            cmbSpol.Name = "cmbSpol";
+            cmbSpol.Size = new Size(168, 23);
+            cmbSpol.TabIndex = 2;
+            cmbSpol.SelectionChangeCommitted += cmbSpol_SelectionChangeCommitted;
+            // 
+            // dgvStudenti
+            // 
+            dgvStudenti.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvStudenti.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvStudenti.Columns.AddRange(new DataGridViewColumn[] { colImePrezime, colDrzava, colGrad, colSpol, colAktivan, colRazmjene });
+            dgvStudenti.Location = new Point(12, 68);
+            dgvStudenti.Name = "dgvStudenti";
+            dgvStudenti.Size = new Size(776, 370);
+            dgvStudenti.TabIndex = 3;
+            dgvStudenti.CellContentClick += dgvStudenti_CellContentClick;
+            dgvStudenti.CellFormatting += dgvStudenti_CellFormatting;
+            // 
+            // colImePrezime
+            // 
+            colImePrezime.HeaderText = "(Indeks) Ime i prezime";
+            colImePrezime.Name = "colImePrezime";
+            // 
+            // colDrzava
+            // 
+            colDrzava.HeaderText = "Država";
+            colDrzava.Name = "colDrzava";
+            // 
+            // colGrad
+            // 
+            colGrad.HeaderText = "Grad";
+            colGrad.Name = "colGrad";
+            // 
+            // colSpol
+            // 
+            colSpol.HeaderText = "Spol";
+            colSpol.Name = "colSpol";
+            // 
+            // colAktivan
+            // 
+            colAktivan.HeaderText = "Aktivan";
+            colAktivan.Name = "colAktivan";
+            // 
+            // colRazmjene
+            // 
+            colRazmjene.HeaderText = "";
+            colRazmjene.Name = "colRazmjene";
+            colRazmjene.Text = "Razmjene";
+            colRazmjene.UseColumnTextForButtonValue = true;
+            // 
+            // lblImePrezime
+            // 
+            lblImePrezime.AutoSize = true;
+            lblImePrezime.Location = new Point(12, 21);
+            lblImePrezime.Name = "lblImePrezime";
+            lblImePrezime.Size = new Size(87, 15);
+            lblImePrezime.TabIndex = 4;
+            lblImePrezime.Text = "Ime ili prezime:";
+            // 
+            // lblDrzava
+            // 
+            lblDrzava.AutoSize = true;
+            lblDrzava.Location = new Point(207, 21);
+            lblDrzava.Name = "lblDrzava";
+            lblDrzava.Size = new Size(45, 15);
+            lblDrzava.TabIndex = 5;
+            lblDrzava.Text = "Država:";
+            // 
+            // lblSpol
+            // 
+            lblSpol.AutoSize = true;
+            lblSpol.Location = new Point(394, 21);
+            lblSpol.Name = "lblSpol";
+            lblSpol.Size = new Size(33, 15);
+            lblSpol.TabIndex = 6;
+            lblSpol.Text = "Spol:";
+            // 
+            // frmPretragaBrojIndeksa
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblSpol);
+            Controls.Add(lblDrzava);
+            Controls.Add(lblImePrezime);
+            Controls.Add(dgvStudenti);
+            Controls.Add(cmbSpol);
+            Controls.Add(cmbDrzava);
+            Controls.Add(txtImePrezime);
+            Name = "frmPretragaBrojIndeksa";
+            Text = "frmPretragaBrojIndeksa";
+            Load += frmPretragaBrojIndeksa_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvStudenti).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtImePrezime;
+        private ComboBox cmbDrzava;
+        private ComboBox cmbSpol;
+        private DataGridView dgvStudenti;
+        private Label lblImePrezime;
+        private Label lblDrzava;
+        private Label lblSpol;
+        private DataGridViewTextBoxColumn colImePrezime;
+        private DataGridViewTextBoxColumn colDrzava;
+        private DataGridViewTextBoxColumn colGrad;
+        private DataGridViewTextBoxColumn colSpol;
+        private DataGridViewCheckBoxColumn colAktivan;
+        private DataGridViewButtonColumn colRazmjene;
+    }
+}
---
+++ PR_III/Exams/PR3-Attempts/Personal/PRIII_30012025_G1_VI/DLWMS.WinApp/IspitBrojIndeksa/frmPretragaBrojIndeksa.cs
@@ -0,0 +1,118 @@
+﻿using DLWMS.Data;
+using DLWMS.Infrastructure;
+using DLWMS.WinApp.Helpers;
+using DocumentFormat.OpenXml.InkML;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DLWMS.WinApp.IspitBrojIndeksa
+{
+    public partial class frmPretragaBrojIndeksa : Form
+    {
+        DLWMSContext db = new DLWMSContext();
+
+        public frmPretragaBrojIndeksa()
+        {
+            InitializeComponent();
+            dgvStudenti.AutoGenerateColumns = false;
+        }
+
+        void FiltrirajStudente()
+        {
+            var query = db.Studenti.Include(s => s.Grad).Include(s => s.Spol).AsQueryable();
+
+            if (cmbDrzava.SelectedIndex >= 0 && cmbDrzava.SelectedValue != null)
+            {
+                query = query.Where(s => s.Grad.DrzavaId == (int)cmbDrzava.SelectedValue);
+            }
+
+            if (cmbSpol.SelectedIndex >= 0 && cmbSpol.SelectedValue != null)
+            {
+                query = query.Where(s => s.Spol.Id == (int)cmbSpol.SelectedValue);
+            }
+
+            var pretragaImePrezime = txtImePrezime.Text.ToLower().Trim();
+            query = query.Where((s) => s.Ime.ToLower().Trim().Contains(pretragaImePrezime));
+
+            this.Text = $"Broj prikazanih studenata: {query.ToList().Count()}";
+
+            dgvStudenti.DataSource = query.ToList();
+
+            colAktivan.DataPropertyName = "Aktivan";
+
+            if (query.ToList().Count() == 0)
+            {
+                MessageBox.Show($"U bazi nisu evidentirani studenti spola {cmbSpol.Text}, koji u imenu i prezimenu posjeduju sadržaj {pretragaImePrezime}, a koji su državljani {cmbDrzava.Text}");
+            }
+        }
+
+        private void frmPretragaBrojIndeksa_Load(object sender, EventArgs e)
+        {
+            cmbDrzava.UcitajPodatke(db.Drzave.ToList());
+            cmbDrzava.SelectedIndex = -1;
+
+            cmbSpol.UcitajPodatke(db.Spolovi.ToList());
+            cmbSpol.SelectedIndex = -1;
+
+            FiltrirajStudente();
+        }
+
+        private void cmbDrzava_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            FiltrirajStudente();
+        }
+
+        private void cmbSpol_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            FiltrirajStudente();
+        }
+
+        private void txtImePrezime_KeyUp(object sender, KeyEventArgs e)
+        {
+            FiltrirajStudente();
+        }
+
+        private void dgvStudenti_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            var student = dgvStudenti.Rows[e.RowIndex].DataBoundItem as Student;
+            var kolona = dgvStudenti.Columns[e.ColumnIndex].Name;
+
+            if (student != null)
+            {
+                if (kolona == "colImePrezime")
+                {
+                    e.Value = $"({student.BrojIndeksa}) {student.Ime} {student.Prezime}";
+                }
+                else if (kolona == "colDrzava")
+                {
+                    e.Value = student.Grad.Drzava.Naziv;
+                }
+                else if (kolona == "colGrad")
+                {
+                    e.Value = student.Grad.Naziv;
+                }
+                else if (kolona == "colSpol")
+                {
+                    e.Value = student.Spol.Naziv;
+                }
+            }
+        }
+
+        private void dgvStudenti_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && dgvStudenti.Columns[e.ColumnIndex].Name == "colAktivan")
+            {
+                dgvStudenti.EndEdit();
+                db.SaveChanges();
+            }
+        }
+    }
+}
