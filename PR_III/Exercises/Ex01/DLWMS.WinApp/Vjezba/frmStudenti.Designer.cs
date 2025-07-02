e6964db2e1ccbe85df93686cc508b2744722c43b
alojsb <58426780+alojsb@users.noreply.github.com>
2025-07-02 22:40:22 +0200

    Ex01

---
+++ PR_III/Exercises/Ex01/DLWMS.WinApp/Vjezba/frmStudenti.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace DLWMS.WinApp.Vjezba
+{
+    partial class frmStudenti
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvStudenti = new DataGridView();
+            ImePrezime = new DataGridViewTextBoxColumn();
+            DatumRodjenja = new DataGridViewTextBoxColumn();
+            Email = new DataGridViewTextBoxColumn();
+            Grad = new DataGridViewTextBoxColumn();
+            Spol = new DataGridViewTextBoxColumn();
+            Aktivan = new DataGridViewCheckBoxColumn();
+            Slika = new DataGridViewImageColumn();
+            ((System.ComponentModel.ISupportInitialize)dgvStudenti).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvStudenti
+            // 
+            dgvStudenti.AllowUserToAddRows = false;
+            dgvStudenti.AllowUserToDeleteRows = false;
+            dgvStudenti.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvStudenti.Columns.AddRange(new DataGridViewColumn[] { ImePrezime, DatumRodjenja, Email, Grad, Spol, Aktivan, Slika });
+            dgvStudenti.Location = new Point(12, 68);
+            dgvStudenti.Name = "dgvStudenti";
+            dgvStudenti.ReadOnly = true;
+            dgvStudenti.Size = new Size(776, 370);
+            dgvStudenti.TabIndex = 0;
+            // 
+            // ImePrezime
+            // 
+            ImePrezime.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            ImePrezime.DataPropertyName = "Prikaz";
+            ImePrezime.HeaderText = "(Indeks) Ime i prezime";
+            ImePrezime.Name = "ImePrezime";
+            ImePrezime.ReadOnly = true;
+            // 
+            // DatumRodjenja
+            // 
+            DatumRodjenja.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            DatumRodjenja.DataPropertyName = "DatumRodjenja";
+            DatumRodjenja.HeaderText = "Datum rođenja";
+            DatumRodjenja.Name = "DatumRodjenja";
+            DatumRodjenja.ReadOnly = true;
+            // 
+            // Email
+            // 
+            Email.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Email.DataPropertyName = "Email";
+            Email.HeaderText = "Email";
+            Email.Name = "Email";
+            Email.ReadOnly = true;
+            // 
+            // Grad
+            // 
+            Grad.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Grad.DataPropertyName = "Grad";
+            Grad.HeaderText = "Grad";
+            Grad.Name = "Grad";
+            Grad.ReadOnly = true;
+            // 
+            // Spol
+            // 
+            Spol.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Spol.DataPropertyName = "Spol";
+            Spol.HeaderText = "Spol";
+            Spol.Name = "Spol";
+            Spol.ReadOnly = true;
+            // 
+            // Aktivan
+            // 
+            Aktivan.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Aktivan.DataPropertyName = "Aktivan";
+            Aktivan.HeaderText = "Aktivan";
+            Aktivan.Name = "Aktivan";
+            Aktivan.ReadOnly = true;
+            // 
+            // Slika
+            // 
+            Slika.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Slika.DataPropertyName = "Slika";
+            Slika.HeaderText = "Profilna slika";
+            Slika.Name = "Slika";
+            Slika.ReadOnly = true;
+            // 
+            // frmStudenti
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dgvStudenti);
+            Name = "frmStudenti";
+            Text = "frmStudenti";
+            Load += frmStudenti_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvStudenti).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dgvStudenti;
+        private DataGridViewTextBoxColumn ImePrezime;
+        private DataGridViewTextBoxColumn DatumRodjenja;
+        private DataGridViewTextBoxColumn Email;
+        private DataGridViewTextBoxColumn Grad;
+        private DataGridViewTextBoxColumn Spol;
+        private DataGridViewCheckBoxColumn Aktivan;
+        private DataGridViewImageColumn Slika;
+    }
+}
---
+++ PR_III/Exercises/Ex01/DLWMS.WinApp/Vjezba/frmStudenti.cs
@@ -0,0 +1,33 @@
+﻿using DLWMS.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DLWMS.WinApp.Vjezba
+{
+    public partial class frmStudenti : Form
+    {
+        DLWMSContext _db = new DLWMSContext();
+
+        public frmStudenti()
+        {
+            InitializeComponent();
+            dgvStudenti.AutoGenerateColumns = false;
+        }
+
+        private void frmStudenti_Load(object sender, EventArgs e)
+        {
+            dgvStudenti.DataSource = _db.Studenti
+                .Include(s => s.Grad)
+                .Include(s => s.Spol)
+                .ToList();
+        }
+    }
+}
