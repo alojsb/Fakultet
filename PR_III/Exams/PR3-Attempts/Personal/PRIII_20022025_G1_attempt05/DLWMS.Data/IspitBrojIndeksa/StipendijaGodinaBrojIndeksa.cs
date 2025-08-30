021f6aa9b0437d4c3a4ae11cd9c7c233cbb8333f
alojsb <58426780+alojsb@users.noreply.github.com>
2025-08-30 21:09:58 +0200

    PRIII_20022025_G1_attempt05
    37 bodova

---
+++ PR_III/Exams/PR3-Attempts/Personal/PRIII_20022025_G1_attempt05/DLWMS.Data/IspitBrojIndeksa/StipendijaGodinaBrojIndeksa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLWMS.Data.IspitBrojIndeksa
+{
+    public class StipendijaGodinaBrojIndeksa
+    {
+        public int Id { get; set; }
+        public int StipendijaId { get; set; }
+        public int Godina {  get; set; }
+        public int MjesecniIznos {  get; set; }
+
+        public StipendijaBrojIndeksa Stipendija { get; set; }
+    }
+}
