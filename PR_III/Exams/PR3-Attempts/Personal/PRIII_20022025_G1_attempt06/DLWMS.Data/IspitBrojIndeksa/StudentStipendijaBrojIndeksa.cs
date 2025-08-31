175a5f966db08374a626664ee2f1e40f55d1f764
alojsb <58426780+alojsb@users.noreply.github.com>
2025-08-31 10:30:01 +0200

    PRIII_20022025_G1_attempt06
    10 bodova

---
+++ PR_III/Exams/PR3-Attempts/Personal/PRIII_20022025_G1_attempt06/DLWMS.Data/IspitBrojIndeksa/StudentStipendijaBrojIndeksa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLWMS.Data.IspitBrojIndeksa
+{
+    public class StudentStipendijaBrojIndeksa
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public int StudentStipendijaId { get; set; }
+
+        public Student Student {  get; set; }
+        public StudentStipendijaBrojIndeksa StudentStipendija { get; set; }
+    }
+}
