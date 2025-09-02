23411e7a1186df192eb8b0dfca520ab4573a0462
alojsb <58426780+alojsb@users.noreply.github.com>
2025-09-02 21:11:45 +0200

    PRIII_20022025_G1_attempt06
    47 bodova

---
+++ PR_III/Exams/PR3-Attempts/Personal/PRIII_20022025_G1_attempt06/DLWMS.WinApp/IspitBrojIndeksa/frmStipendijaAddEditBrojIndeksa.cs
@@ -0,0 +1,116 @@
+﻿using DLWMS.Data;
+using DLWMS.Data.IspitBrojIndeksa;
+using DLWMS.Infrastructure;
+using DLWMS.WinApp.Helpers;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DLWMS.WinApp.IspitBrojIndeksa
+{
+    public partial class frmStipendijaAddEditBrojIndeksa : Form
+    {
+        DLWMSContext db;
+        StudentStipendijaBrojIndeksa ss;
+        bool isEditMode;
+
+        public frmStipendijaAddEditBrojIndeksa(DLWMSContext context, StudentStipendijaBrojIndeksa studentStipendija = null)
+        {
+            InitializeComponent();
+            db = context;
+            ss = studentStipendija;
+            isEditMode = studentStipendija != null;
+        }
+
+        private void frmStipendijaAddEditBrojIndeksa_Load(object sender, EventArgs e)
+        {
+            if (isEditMode)
+            {
+                var student = ss.Student;
+                cmbStudent.DataSource = new List<Student> { student };
+                cmbStudent.ValueMember = "Id";
+                cmbStudent.DisplayMember = "Prikaz";
+                cmbStudent.Enabled = false;
+
+                cmbGodina.SelectedItem = ss.StipendijaGodina.Godina.ToString();
+
+                OsvjeziStipendije();
+                cmbStipendija.SelectedValue = ss.StipendijaGodina.StipendijaId;
+            }
+            else
+            {
+                cmbStudent.UcitajPodatke(db.Studenti.ToList());
+                cmbStudent.DisplayMember = "Prikaz";
+
+                cmbGodina.SelectedIndex = 0;
+
+                OsvjeziStipendije();
+            }
+        }
+
+        private void OsvjeziStipendije()
+        {
+            var odabranaGodina = int.Parse(cmbGodina.Text);
+
+            var stipendijeZaGodinu = db.StipendijeGodineBrojIndeksa
+                .Include(sg => sg.Stipendija)
+                .Where(sg => sg.Godina == odabranaGodina)
+                .Select(sg => sg.Stipendija)
+                .ToList();
+
+            cmbStipendija.UcitajPodatke(stipendijeZaGodinu);
+        }
+
+        private void btnSacuvaj_Click(object sender, EventArgs e)
+        {
+            var studentId = cmbStudent.SelectedValue as int?;
+            var godinaText = cmbGodina.SelectedItem?.ToString();
+            var stipendijaId = cmbStipendija.SelectedValue as int?;
+
+            if (studentId == null || stipendijaId == null || string.IsNullOrWhiteSpace(godinaText))
+            {
+                MessageBox.Show("Molimo unesite vrijednosti za polja student, godina i stipendija.");
+                return;
+            }
+
+            var godina = int.Parse(godinaText);
+
+            var stipendijaGodina = db.StipendijeGodineBrojIndeksa
+                .FirstOrDefault(sg => sg.Godina == godina && sg.StipendijaId == stipendijaId);
+
+            bool isDuplikat = db.StudentiStipendijeBrojIndeksa
+                .Any(item => item.StudentId == studentId && item.StipendijaGodinaId == stipendijaGodina.Id &&
+                    (!isEditMode || item.Id != ss.Id));
+
+            if (isDuplikat)
+            {
+                MessageBox.Show("Za navedenomg studenta već postoji zapis o odabranoj stipendiji.");
+                return;
+            }
+
+            if (isEditMode)
+            {
+                ss.StipendijaGodinaId = stipendijaGodina.Id;
+            }
+            else
+            {
+                var novaStudentStipendija = new StudentStipendijaBrojIndeksa
+                {
+                    StudentId = studentId.Value,
+                    StipendijaGodinaId = stipendijaGodina.Id
+                };
+                db.StudentiStipendijeBrojIndeksa.Add(novaStudentStipendija);
+            }
+            db.SaveChanges();
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+    }
+}
