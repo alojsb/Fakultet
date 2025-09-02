25ef6a8e1b76457108bfcdf482ff85e8904d6eb1
alojsb <58426780+alojsb@users.noreply.github.com>
2025-09-02 21:42:06 +0200

    PRIII_20022025_G1_attempt07
    10 bodova

---
+++ PR_III/Exams/PR3-Attempts/Personal/PRIII_20022025_G1_attempt07/DLWMS.Infrastructure/DLWMSContext.cs
@@ -0,0 +1,23 @@
+﻿using DLWMS.Data;
+using DLWMS.Data.IspitBrojIndeksa;
+using Microsoft.EntityFrameworkCore;
+
+using System.Configuration;
+
+namespace DLWMS.Infrastructure
+{
+    public class DLWMSContext : DbContext
+    {
+      
+        private string konekcijskiString = ConfigurationManager.ConnectionStrings["DLWMSBaza"].ConnectionString;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(konekcijskiString);
+        }
+
+        public DbSet<Student> Studenti { get; set; }
+        public DbSet<StipendijaBrojIndeksa> StipendijeBrojIndeksa { get; set; }
+        public DbSet<StipendijaGodinaBrojIndeksa> StipendijeGodineBrojIndeksa { get; set; }
+        public DbSet<StudentStipendijaBrojIndeksa> StudentiStipendijeBrojIndeksa { get; set; }
+    }
+}
